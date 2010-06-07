7a35dc5c8b146245c78c94cc488c75cf57ffd0e9
nano.seabra <nano.seabra@15dc817e-3944-cecd-dbe3-d4ae5dc8051f>
2010-06-07 19:15:22 +0000

    - Adicionado ao FtpClient o recurso de verificar e armazenar a data de modificação de cada arquivo (Renan).

--- trunk/FtpClient/src/ftpclient/AirportsRequester.java
+++ trunk/FtpClient/src/ftpclient/AirportsRequester.java
@@ -8,6 +8,9 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONException;
+import org.json.JSONTokener;
+
 /**
  * AirportsRequester gets the list of airports from the server, through a GET
  * method.
@@ -23,16 +26,20 @@ public class AirportsRequester {
 	/**
 	 * Constructor of AirportsRequester.
 	 */
-	public AirportsRequester() throws IOException {
+	public AirportsRequester() throws IOException, JSONException {
 		URL url = new URL("http://localhost:8888/airports");
 		URLConnection connection = url.openConnection();
 		BufferedReader in = new BufferedReader(new InputStreamReader(
 				connection.getInputStream()));
 		String input;
-		while ((input = in.readLine()) != null) {
-			airports.add(input);
-		}
+		JSONTokener jsonT = new JSONTokener(in.readLine());
+		//while ((input = in.readLine()) != null) {
+		//	airports.add(input);
+		//}
 		in.close();
+		while (jsonT.more()) {
+			System.out.println(jsonT.nextValue());
+		}
 	}
 	
 	/**
